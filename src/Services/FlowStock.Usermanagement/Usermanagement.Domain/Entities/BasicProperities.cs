3dd6393f0e0b4a366917f579b42422a6f6ad74ae
Ali Fakhri <36810715+Ali7495@users.noreply.github.com>
2026-06-16 13:27:59 -0700

    a file deleted

--- src/Services/FlowStock.Usermanagement/Usermanagement.Domain/Entities/BasicProperities.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Usermanagement.Domain;
-
-public abstract class BasicProperities
-{
-    public Guid Id { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public DateTime UpdatedAt { get; set; }
-    public bool IsDeleted { get; set; }
-}
