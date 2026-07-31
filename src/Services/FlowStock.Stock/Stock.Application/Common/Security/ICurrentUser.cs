75f9931cfd26d23447864bd09d77b689e6ccb731
Ali Fakhri <36810715+Ali7495@users.noreply.github.com>
2026-07-31 13:59:30 -0700

    jwt handling

---
+++ src/Services/FlowStock.Stock/Stock.Application/Common/Security/ICurrentUser.cs
@@ -0,0 +1,11 @@
+﻿namespace Stock.Application;
+
+public interface ICurrentUser
+{
+    bool IsAuthenticated { get; }
+    Guid PersonId { get; }
+    Guid UserId { get; }
+    string Username { get; }
+    IReadOnlyCollection<string> Roles { get; }
+    bool IsInRole(string role);
+}
---
+++ src/Services/FlowStock.Stock/Stock.Infrastructure/Security/CurrentUser.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using Stock.Application;
+
+namespace Stock.Infrastructure;
+
+public sealed class CurrentUser : ICurrentUser
+{
+
+    private readonly IHttpContextAccessor _context;
+
+    public CurrentUser(IHttpContextAccessor context)
+    {
+        _context = context;
+    }
+
+    private ClaimsPrincipal User => _context.HttpContext?.User!;
+    
+    public bool IsAuthenticated => User.Identity?.IsAuthenticated ?? false;
+
+    public Guid PersonId => Guid.Parse(User.FindFirst("sub")!.Value);
+
+    public Guid UserId => Guid.Parse(User.FindFirst("userId")!.Value);
+
+    public string Username => User.Identity?.Name ?? string.Empty;
+
+    public IReadOnlyCollection<string> Roles => User.FindAll(ClaimTypes.Role).Select(r=> r.Value).ToList();
+
+    public bool IsInRole(string role)
+    {
+        return Roles.Contains(role);
+    }
+}
