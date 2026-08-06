cdfc487f8c4a2e82d1c12d356920669f5f95c7d4
Ali Fakhri <36810715+Ali7495@users.noreply.github.com>
2026-08-06 11:00:29 -0700

    Tests added

---
+++ src/Tests/FlowStock.Stock/Stock.Application.UnitTests/Commands/ProductCategory/Create/CreateProductCategoryCommandHandlerTests.cs
@@ -0,0 +1,14 @@
+﻿namespace Stock.Application.UnitTests;
+
+public class CreateProductCategoryCommandHandlerTests
+{
+    [Fact]
+    public async Task Should_Create_ProductCategory_When_Command_Is_Valid()
+    {
+        //Arrange
+
+        //Act
+
+        //Assert
+    }
+}
