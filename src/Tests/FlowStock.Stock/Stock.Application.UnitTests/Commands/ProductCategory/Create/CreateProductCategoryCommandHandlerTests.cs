674e73b573e9dffa0dbf46b514e6003e6fb4c1e9
Ali Fakhri <36810715+Ali7495@users.noreply.github.com>
2026-08-07 13:00:17 -0700

    Test just begun (#12)

--- src/Tests/FlowStock.Stock/Stock.Application.UnitTests/Commands/ProductCategory/Create/CreateProductCategoryCommandHandlerTests.cs
+++ src/Tests/FlowStock.Stock/Stock.Application.UnitTests/Commands/ProductCategory/Create/CreateProductCategoryCommandHandlerTests.cs
@@ -1,4 +1,9 @@
-﻿namespace Stock.Application.UnitTests;
+﻿using BuildingBlocks.Application;
+using FluentAssertions;
+using Moq;
+using Stock.Domain;
+
+namespace Stock.Application.UnitTests;
 
 public class CreateProductCategoryCommandHandlerTests
 {
@@ -6,9 +11,25 @@ public class CreateProductCategoryCommandHandlerTests
     public async Task Should_Create_ProductCategory_When_Command_Is_Valid()
     {
         //Arrange
+        Mock<IProductCategoryRepository> repository = new ();
+
+        repository.Setup(p=> p.ExistsByNameAsync("Gold",It.IsAny<CancellationToken>())).ReturnsAsync(false);
+
+        Mock<ICurrentUser> currentUser = new();
+
+        currentUser.Setup(c=> c.PersonId).Returns(Guid.NewGuid);
+
+        CreateProductCategoryCommandHandler handler = new(repository.Object, currentUser.Object);
+
+        CreateProductCategoryCommand command = new("Gold");
 
         //Act 
 
+        Guid id = await handler.Handle(command, CancellationToken.None);
+
         //Assert
+
+        id.Should().NotBeEmpty();
+        repository.Verify(x=> x.AddAsync(It.IsAny<ProductCategory>(), It.IsAny<CancellationToken>()), Times.Once);
     }
 }
