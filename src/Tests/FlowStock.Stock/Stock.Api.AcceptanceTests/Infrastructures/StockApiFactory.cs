e368043e5bb45ad17a461d886b14eb2817a18a12
Ali Fakhri <36810715+Ali7495@users.noreply.github.com>
2026-09-18 19:56:44 +0330

    (#27) Before & After scenarios with factory and test db

---
+++ src/Tests/FlowStock.Stock/Stock.Api.AcceptanceTests/Infrastructures/StockApiFactory.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Npgsql;
+using Stock.Infrastructure;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Stock.Api.AcceptanceTests;
+
+public sealed class StockApiFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.UseEnvironment("Testing");
+
+        string connectionString = Environment.GetEnvironmentVariable("FLOWSTOCK_TEST_DB")
+            ?? throw new InvalidOperationException("FLOWSTOCK_TEST_DB is not exist!");
+
+        NpgsqlConnectionStringBuilder connection = new(connectionString);
+
+        if (connection.Database != "StockAcceptanceTestsDb")
+        {
+            throw new InvalidOperationException("Acceptance tests must use StockAcceptanceTestsDb!");
+        }
+
+        builder.ConfigureServices(services =>
+       {
+           services.RemoveAll<StockDbContext>();
+
+           services.RemoveAll<DbContextOptions<StockDbContext>>();
+
+           services.RemoveAll<IDbContextOptionsConfiguration<StockDbContext>>();
+
+
+
+           services.AddDbContext<StockDbContext>(options =>
+           {
+               options.UseNpgsql(connectionString);
+           });
+       });
+    }
+}
