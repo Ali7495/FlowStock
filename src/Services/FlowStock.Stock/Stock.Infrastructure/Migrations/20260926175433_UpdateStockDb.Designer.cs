43b9c625ccc507473fdbed24222908ed12567135
Ali Fakhri <36810715+Ali7495@users.noreply.github.com>
2026-09-26 21:28:33 +0330

    (#33 #35 #37) TDD completed.

--- src/Services/FlowStock.Stock/Stock.Infrastructure/Migrations/20260926175433_UpdateStockDb.Designer.cs
+++ src/Services/FlowStock.Stock/Stock.Infrastructure/Migrations/20260926175433_UpdateStockDb.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Stock.Infrastructure;
@@ -11,9 +12,11 @@ using Stock.Infrastructure;
 namespace Stock.Infrastructure.Migrations
 {
     [DbContext(typeof(StockDbContext))]
-    partial class StockDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260926175433_UpdateStockDb")]
+    partial class UpdateStockDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
